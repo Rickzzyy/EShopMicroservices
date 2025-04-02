53141897fda874121345dd9aa276d316207069f3
Ricards Taukuls <ricards.taukuls@inbox.lv>
2025-04-02 11:50:11 +0300

    Test Catalog API - CreateProduct POST Request - Debug Carter Minimal API and Med

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,27 +1,29 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
 public record CreateProductResponse(Guid Id);
 
 public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
-        {
-            var command = request.Adapt<CreateProductCommand>();
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
 
-            var result = await sender.Send(command);
-            var response = result.Adapt<CreateProductResponse>();
+                var result = await sender.Send(command);
 
-            return Results.Created($"/products/{response.Id}", response);
+                var response = result.Adapt<CreateProductResponse>();
 
-        })
-            .WithName("CreateProduct")
-            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
-            .ProducesProblem(StatusCodes.Status400BadRequest)
-            .WithSummary("Create product")
-            .WithDescription("reate product");
+                return Results.Created($"/products/{response.Id}", response);
 
+            })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
