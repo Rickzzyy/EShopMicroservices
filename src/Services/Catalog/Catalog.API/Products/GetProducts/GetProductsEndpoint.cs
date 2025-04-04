7c514050981b5d162f032c378ed4213bdcc0c9c9
Ricards Taukuls <ricards.taukuls@inbox.lv>
2025-04-04 10:29:08 +0300

    Develop Getproductes GET Endpoint with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,27 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+
+//public record GetProductsRequest();
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
 {
-    public class GetProductsEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products",
+            async (ISender sender) =>
+            {
+                var query = new GetProductsQuery();
+                var result = await sender.Send(query);
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+             .WithName("GetProducts")
+             .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .WithSummary("Get Products")
+             .WithDescription("Get Products");
     }
 }
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,22 +1,18 @@
-﻿using Marten.Linq.QueryHandlers;
+﻿namespace Catalog.API.Products.GetProducts;
 
-namespace Catalog.API.Products.GetProducts
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+internal class GetProductsQueryHandler
+    (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
-    public record GetProductsResult(IEnumerable<Product> Products);
-    internal class GetProductsQueryHandler
-        (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
-        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
-        {
 
-            logger.LogInformation("GetProductsQueryHandler.Handle called {@Query}",query );
-           
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        logger.LogInformation("GetProductsQueryHandler.Handle called {@Query}",query );
+       
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
 
-            return new GetProductsResult(products);
-        }
+        return new GetProductsResult(products);
     }
 }
