26d769edc0e230916cff87e8301b53bb121d5ff7
ricards <ricards.taukuls@inbox.lv>
2025-04-05 12:33:23 +0300

    Global Exception Handling with IExceptionHandler interface in Asp.net Core

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,8 +1,22 @@
 ﻿
+using Catalog.API.Products.CreateProduct;
+
 namespace Catalog.API.Products.UpdateProduct;
 
 public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile,decimal Price  ) : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.").Length(2,150).WithMessage("Name must be between 2 and 150 characters");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+    }
+}
+
+
 public class UpdateProductCommandHandler
     (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
@@ -14,7 +28,7 @@ public class UpdateProductCommandHandler
         
         if(product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(command.Id);
         }
 
         product.Name = command.Name;
