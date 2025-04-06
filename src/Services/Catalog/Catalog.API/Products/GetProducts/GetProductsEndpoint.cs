717f5b0580869715fb1c1656e4e8c120a2a14bbf
ricards <ricards.taukuls@inbox.lv>
2025-04-06 10:40:13 +0300

    Develop Pagination for GetProducts Query with ToPagedListAsync extention method

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -2,7 +2,7 @@
 namespace Catalog.API.Products.GetProducts;
 
 
-//public record GetProductsRequest();
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 
 public record GetProductsResponse(IEnumerable<Product> Products);
 
@@ -11,9 +11,9 @@ public class GetProductsEndpoint : ICarterModule
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapGet("/products",
-            async (ISender sender) =>
+            async ([AsParameters] GetProductsRequest request, ISender sender) =>
             {
-                var query = new GetProductsQuery();
+                var query = request.Adapt<GetProductsQuery>();
                 var result = await sender.Send(query);
                 var response = result.Adapt<GetProductsResponse>();
                 return Results.Ok(response);
