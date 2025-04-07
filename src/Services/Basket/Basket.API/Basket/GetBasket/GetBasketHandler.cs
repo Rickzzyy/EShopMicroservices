0673d4403e8f0b8463256e2ddad35ae63fac531b
Ricards Taukuls <ricards.taukuls@inbox.lv>
2025-04-07 10:42:16 +0300

    Develop GetBasket Handler and Endpoint with minimal Apis and Carter

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,17 @@
-﻿namespace Basket.API.Basket.GetBasket;
+﻿
+namespace Basket.API.Basket.GetBasket;
 
-public class GetbAsketHandler
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //TODO: get basket from db
+        //var basket = await _repository.GetBasket(query.UserName);
+        return new GetBasketResult(new ShoppingCart("swn"));
+
+    }
 }
