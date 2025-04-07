c982a1dc619db3406003198dc815348dafbdf18c
Ricards Taukuls <ricards.taukuls@inbox.lv>
2025-04-07 11:09:48 +0300

    Develop Deletebasket Handler, Endpoint. Register MediatR and Carter libraries into Asp.Net Dependency

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeletBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeletBasketHandler.cs
@@ -1,5 +1,22 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeletBasketHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+public class DeletBasketCommandHandler 
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+       //TODO: delete basket from db and cache
+       //session.Delete<Product>(command.Id)
+       return new DeleteBasketResult(true);
+    }
 }
--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,5 +1,23 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿
+namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketEndpoint
+//public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
+public class DeleteBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("basket/{UserName}", async (string UserName, ISender sender)=>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(UserName));
+            var response = result.Adapt<DeleteBasketResponse>();
+            return Results.Ok(response);
+        })
+            .WithName("DeleteBasket")
+            .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+    }
 }
