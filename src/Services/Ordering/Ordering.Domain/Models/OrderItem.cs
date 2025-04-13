c65856114e9dc6caf67f83b90c5deaf995032996
ricards <ricards.taukuls@inbox.lv>
2025-04-13 12:56:52 +0300

    Develop DDD Rich-domain model for Order Entities in Order.Domain.Layer

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -2,7 +2,7 @@
 
 public class OrderItem : Entity<OrderItemId>
 {
-    internal OrderItem(OrderId orderId, ProductId productId, decimal price, int quantity)
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
     {
         Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
