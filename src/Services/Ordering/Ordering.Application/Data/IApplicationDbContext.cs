29a2e0c3420490f3cb51ed175013236acf9daf21
Ricards Taukuls <ricards.taukuls@inbox.lv>
2025-05-07 11:13:19 +0300

    Develop IApplicationDbContext for injection EFCore DB Context object in Ordering

--- src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -1,5 +1,14 @@
-﻿namespace Ordering.Application.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
 
-interface IApplicationDbContext
+
+namespace Ordering.Application.Data;
+public interface IApplicationDbContext
 {
+    DbSet<Customer> Customers { get; }
+    DbSet<Product> Products { get; }
+    DbSet<Order> Orders { get; }
+    DbSet<OrderItem> OrderItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
