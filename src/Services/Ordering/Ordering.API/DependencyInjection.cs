688174372f9536c10f5f070b54c54d5f06865c67
Ricards Taukuls <ricards.taukuls@inbox.lv>
2025-05-13 11:40:29 +0300

    Cross-Cutting Concerns Custom Exception Handler in Ordering Microservice

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,4 +1,4 @@
-﻿using Carter;
+﻿using BuildingBlocks.Exceptions.Handler;
 
 namespace Ordering.API;
 
@@ -7,13 +7,14 @@ public static class DependencyInjection
     public static IServiceCollection AddApiSerivces(this IServiceCollection services)
     {
         services.AddCarter();
-
+        services.AddExceptionHandler<CustomExceptionHandler>();
         return services;
     }
 
     public static WebApplication UseApiSerivces(this WebApplication app)
     {
         app.MapCarter();
+        app.UseExceptionHandler(options => { });
         return app;
     }
 
